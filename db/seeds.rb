1d354e0b83fc8d8ec2059ecbbdb5036f65ddfd0e
ramonlg <ramonlopes.87@gmail.com>
2017-09-21 22:03:51 -0300

    Create Ads model and add rails-erd gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+puts "Cadastrando as CATEGORIAS..."
 categories = [
     "Animais e acessórios",
     "Esportes",
